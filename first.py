e27aa913bb8fa7ec60bfce6ede948179b08c16cd
Sveta <21.11.1989@list.ru>
2023-06-20 21:17:19 +0300

    ygas

---
+++ first.py
@@ -0,0 +1,17 @@
+
+name = input('Как твое имя?')
+age = int(input('Сколько лет?'))
+
+if age<=30:
+    print(f'Привет, {name}!')
+else:
+    print(f'Пока, {name} - ***')
+
+print('the and')
+
+
+
+
+
+
+
